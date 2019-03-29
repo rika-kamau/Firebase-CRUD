7d7a06e6b55ff57118b523e135bb63bb190e5fc5
rika-kamau <johnnjoguk@gmail.com>
2019-03-29 11:10:35 +0300

    first commit

---
+++ app/src/main/java/javamorning/com/morningfirebaseproject/UsersActivity.java
@@ -0,0 +1,60 @@
+package javamorning.com.morningfirebaseproject;
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class UsersActivity extends AppCompatActivity {
+    ListView list;
+    CustomAdapter adapter;
+    ArrayList<User> users;
+    ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+        list=findViewById(R.id.listPerson);
+        users=new ArrayList<>();
+        adapter=new CustomAdapter(this,users);
+        dialog=new ProgressDialog(this);
+        dialog.setTitle("Loading");
+        dialog.setMessage("Please wait...");
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference().child("Users");
+        dialog.show();
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                users.clear();
+                for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+                    User user=snapshot.getValue(User.class);
+                    users.add(user);
+                    Collections.reverse(users);
+                }
+                adapter.notifyDataSetChanged();
+                dialog.dismiss();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(UsersActivity.this, "Database connection failed!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        list.setAdapter(adapter);
+    }
+}
